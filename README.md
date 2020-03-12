920157013cf84d0a7d604b3ea338b4d74126726c
Alexander Orlov <alexander.orlov@ix.co>
2020-03-12 23:57:02 +0200

    Added Policy Upload

--- README.md
+++ README.md
@@ -6,3 +6,130 @@ In the project directory, you can run:
 
 ### `yarn start`
 
+# POC
+
+To Run the POC you need to 
+- Configure AWS
+- Generate Policy
+- Send Files to the URL adding generated policy configuraton
+
+
+## AWS S3 Bucket configuration
+
+Use this page as an inspiration:
+https://softwareontheroad.com/aws-s3-secure-direct-upload/
+
+### Generate IAM Credentials to get Access Key and Access Secret
+![AWS: How To Get Amazon S3 Access Keys](https://objectivefs.com/howto/how-to-get-amazon-s3-keys)
+
+### Configure AWS Policy
+
+Set the Bucket Policy to
+```
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:Put*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::your-bucket-name/*",
+            ]
+        }
+    ]
+}
+```
+
+:bulb: Make sure you changed `your-bucket-name` to actually your S3 bucket Name
+
+### Configure AWS Policy
+
+As these request will be done through the web, you have to allow cross-origin (CORS) requests on this bucket.
+
+- Go to your bucket
+- Go to the permissions tab
+- Click CORS configuration and copy and paste the following
+
+```
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>*</AllowedOrigin>
+    <AllowedMethod>HEAD</AllowedMethod>
+    <AllowedMethod>GET</AllowedMethod>
+    <AllowedMethod>PUT</AllowedMethod>
+    <AllowedMethod>POST</AllowedMethod>
+    <AllowedHeader>*</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+```
+
+:thinking: Assuming all Methods other than `POST` Can be removed from this snippet
+
+## Generating Signed URL
+
+Install jwplatform NodeJS lib
+```
+$> yarn add aws-sdk
+```
+
+Create NodeJS file `index.js`
+```
+const AWS = require('aws-sdk');
+
+const [accessKeyId, secretAccessKey, Bucket] = [
+    '-- AWS ACCESS KEY --',
+    '-- AWS SECRET KEY --',
+    '-- AWS BUCKET --'
+];
+
+var s3 = new AWS.S3({
+    accessKeyId,
+    secretAccessKey
+});
+
+const uploadPath = "test/ix/s3-upload"
+
+var params = {
+    Bucket,
+};
+
+s3.createPresignedPost(params, (err, data) => {
+    if (err) {
+        console.error('Pre-signing post data encountered an error', err);
+    } else {
+        console.log('The post data is in between lines');
+        console.log('------------');
+        console.log(JSON.stringify(data));
+        console.log('------------');
+    }
+});
+```
+
+Run `index.js` file
+```
+$> node index.js
+```
+
+Copy the Data generated from Terminal console and use it on a second step.
+
+
+## Uploading File to S3
+
+Run React-App application
+```
+$> yarn start
+```
+
+Go to http://localhost:3000
+
+Put the Data in a dialog you see on a screen, it should fill in readonly fields
+
+Select a video File from your local environment
+![Web-View](../assets/img/policy-upload.png?raw=true)
+
+And hit upload
+
+Check the Developer Tools console for any potential issues
--- src/App.js
+++ src/App.js
@@ -1,61 +1,88 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import './App.css';
+import Axios from 'axios';
 
-
-const submitData = async (uploadUrl, {
-    AWSAccessKeyId,
-    Signature,
-}, file, onError) => {
+const submitData = async (uploadUrl, policyFields,
+    file,
+    onError,
+    onSuccess,
+    onProgress
+) => {
     const fd = new FormData();
-    // fd.append('key', key);
+    // // fd.append('key', key);
     // fd.append('acl', 'private');
     fd.append('Content-Type', file.type);
-    fd.append('AWSAccessKeyId', AWSAccessKeyId);
-    fd.append('signature', Signature);
+    fd.append('Key', file.name);
     fd.append('file', file, file.name);
 
-    try {
-        const resp = await fetch(uploadUrl, {
-            body: fd,
-            method: 'PUT',
-        });
-        if (!resp.ok) {
-            throw new Error(`${resp.type} / ${resp.status}:${resp.statusText}`);
-        }
-    } catch (error) {
-        onError(error.toString());
-    }
-};
+    Object.keys(policyFields).forEach(x => {
+        fd.append(x, policyFields[x]);
+    })
 
+    axios.post(uploadUrl, fd, {
+        onUploadProgress: ({loaded, total}) => onProgress((loaded / total) * 100 | 0),
+    }).then(resp => {
+        console.log(resp);
+        if (resp.ok === false) {
+            onError(`${resp.type} / ${resp.status}:${resp.statusText}`);
+        } else {
+            onSuccess();
+        }
+    }).catch(error => {
+        onError(error.toString());
+    })
+};
 
 export default () => {
     const [error, setError] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [progress, setProgress] = useState(0);
 
+
+    const [uploadPolicy, setUploadPolicy] = useState("");
     const [uploadUrl, setUploadUrl] = useState("");
+    const [policyFields, setPolicyFields] = useState({});
+
     const [file, setFile] = useState(null);
-    const [AWSAccessKeyId, setAWS] = useState("");
-    const [Signature, setSignature] = useState("");
-    const [Expires, setExpires] = useState("");
 
-    useEffect(() => {
-        if (uploadUrl) {
-            try {
-                let params = new URLSearchParams((new URL(uploadUrl)).search);
-                setAWS(params.get("AWSAccessKeyId"));
-                setSignature(params.get("Signature"));
-                setExpires(params.get("Expires"));
-            } catch {
-                setError("Wrong URL");
-                return;
-            }
-
-        }
-    }, [uploadUrl]);
 
     const handleSubmit = (event) => {
-        submitData(uploadUrl, {AWSAccessKeyId, Signature}, file, setError);
+        submitData(uploadUrl, policyFields, file,
+            setError,
+            onUploadSuccess,
+            setProgress);
         event.preventDefault();
     };
+
+    const onUploadSuccess = () => {
+        setError(false);
+        setSuccess(true);
+        setTimeout(() => {
+            setSuccess(false);
+            setFile(null);
+            setUploadPolicy("");
+            setUploadUrl("");
+            setPolicyFields({});
+            setProgress(0);
+        }, 5 * 1000);
+    };
+
+    useEffect(() => {
+        if (uploadPolicy) {
+            try {
+                const {url, fields} = JSON.parse(uploadPolicy);
+                setPolicyFields(fields);
+                setUploadUrl(url);
+                setError("");
+
+            } catch (er) {
+                setError("Policy is misformed");
+                return;
+            }
+        }
+    }, [uploadPolicy]);
 
     return (
         <div className="App">
@@ -64,28 +91,54 @@ export default () => {
                 <br />
                 <br />
                 <form onSubmit={handleSubmit}>
-                    {error && (<div className="alert alert-danger" role="alert">
-                        {error}
+                    {error && (<div className="alert alert-danger" role="alert">{error}</div>)}
+                    {success && (<div className="alert alert-success" role="alert">Uploaded</div>)}
+                    {(progress > 0) && (<div className="form-group">
+                        <div className="progress">
+                            <div className="progress-bar bg-info" role="progressbar" style={{width: `${progress}%`}} aria-valuenow={progress} aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
                     </div>)}
                     <div className="form-group">
-                        <input type="text" className="form-control" aria-describedby="emailHelp" placeholder="Upload File URL"
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"
+                            value={uploadPolicy}
+                            onChange={event => setUploadPolicy(event.target.value)}
+                        ></textarea>
+                    </div>
+                    <div className="form-group">
+                        <input className="form-control" type="text" placeholder="Upload Url" disabled
                             value={uploadUrl}
-                            onChange={event => setUploadUrl(event.target.value)}
                         />
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-6">
-                            <input className="form-control" type="text" placeholder="AWSAccessKeyId" readOnly disabled
-                                value={AWSAccessKeyId}
+                            <input className="form-control" type="text" placeholder="Bucket" disabled
+                                value={(policyFields || {})['bucket']}
                             />
                         </div>
                         <div className="form-group col-md-6">
-                            <input className="form-control" type="text" placeholder="Signature" readOnly disabled
-                                value={Signature} />
+                            <input className="form-control" type="text" placeholder="X-Amz-Algorithm" disabled
+                                value={(policyFields || {})['X-Amz-Algorithm']}
+                            />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <input className="form-control" type="text" placeholder="X-Amz-Credential" readOnly disabled
+                                value={(policyFields || {})['X-Amz-Credential']} />
                         </div>
                         <div className="form-group col-md-6">
-                            <input className="form-control" type="text" placeholder="Expires" readOnly disabled
-                                value={Expires && (new Date(Expires * 1000))} />
+                            <input className="form-control" type="text" placeholder="X-Amz-Date" readOnly disabled
+                                value={(policyFields || {})['X-Amz-Date']} />
+                        </div>
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <input className="form-control" type="text" placeholder="Policy" readOnly disabled
+                                value={(policyFields || {})['Policy']} />
+                        </div>
+                        <div className="form-group col-md-6">
+                            <input className="form-control" type="text" placeholder="X-Amz-Signature" readOnly disabled
+                                value={(policyFields || {})['X-Amz-Signature']} />
                         </div>
                     </div>
                     <div className="form-group">
